c72c9ced2143f53b2093327f3d6608ac7bcd5d0f
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2024-12-14 16:06:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+ let admin, name; // Puedes declarar dos variables a la vez.
+
+name = "John";
+  
+admin = name;
+  
+alert( admin );
+
+
+let planeta = "Tierra";
+let userName = "Pedro";
